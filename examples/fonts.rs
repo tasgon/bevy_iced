03d599db0e22b45ec99a83b3aad8226cc25c32da
tasgon <me@tas.sh>
2023-08-08 20:24:29 -0700

    only keep one font; bump version

--- examples/fonts.rs
+++ examples/fonts.rs
@@ -2,13 +2,13 @@ use std::{fs, io};
 
 use bevy::prelude::*;
 use bevy_iced::iced::{
+    font,
     widget::{column, text},
     Font,
 };
 use bevy_iced::{iced, IcedContext, IcedPlugin};
 
 const ALPHAPROTA_FONT: Font = Font::with_name("Alpha Prota");
-const RAINBOW2000_FONT: Font = Font::with_name("Rainbow 2000");
 
 #[derive(Event)]
 pub enum UiMessage {}
@@ -17,13 +17,10 @@ pub fn main() -> io::Result<()> {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_plugins(IcedPlugin {
-            fonts: vec![
-                fs::read("assets/fonts/AlphaProta.ttf")?.leak(),
-                fs::read("assets/fonts/Rainbow2000-Regular.ttf")?.leak(),
-            ],
+            fonts: vec![fs::read("assets/fonts/AlphaProta.ttf")?.leak()],
             settings: iced::Settings {
-                default_font: ALPHAPROTA_FONT,
                 default_text_size: 40.0,
+                default_font: ALPHAPROTA_FONT,
                 ..Default::default()
             },
         })
@@ -36,7 +33,7 @@ pub fn main() -> io::Result<()> {
 
 fn ui_system(mut ctx: IcedContext<UiMessage>) {
     ctx.display(column!(
-        text(format!("I am the default font")),
-        text(format!("I am another font")).font(RAINBOW2000_FONT)
+        text(format!("I am the default font")).font(font::Font::DEFAULT),
+        text(format!("I am another font"))
     ));
 }
