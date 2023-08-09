c9d43fbea81a8fa901709e2d9e3e0b0536788fad
tasgon <me@tas.sh>
2023-08-08 20:42:17 -0700

    fmt

--- src/lib.rs
+++ src/lib.rs
@@ -4,10 +4,10 @@
 //! use bevy::prelude::*;
 //! use bevy_iced::iced::widget::text;
 //! use bevy_iced::{IcedContext, IcedPlugin};
-//! 
+//!
 //! #[derive(Event)]
 //! pub enum UiMessage {}
-//! 
+//!
 //! pub fn main() {
 //!     App::new()
 //!         .add_plugins(DefaultPlugins)
@@ -16,7 +16,7 @@
 //!         .add_systems(Update, ui_system)
 //!         .run();
 //! }
-//! 
+//!
 //! fn ui_system(time: Res<Time>, mut ctx: IcedContext<UiMessage>) {
 //!     ctx.display(text(format!(
 //!         "Hello Iced! Running for {:.2} seconds.",
